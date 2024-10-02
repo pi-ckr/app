5085347cd82c2ccc611393821f118f27a00aa059
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-10-02 21:42:32 +0900

    Initial Commit

---
+++ Pickr/PickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickrApp.swift
+//  Pickr
+//
+//  Created by  jwkwon0817 on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickrTests/PickrTests.swift
@@ -0,0 +1,16 @@
+//
+//  PickrTests.swift
+//  PickrTests
+//
+//  Created by  jwkwon0817 on 10/2/24.
+//
+
+import Testing
+
+struct PickrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
